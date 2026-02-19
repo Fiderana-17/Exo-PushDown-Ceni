b706e66455f4849db57f226d97a9ed36da615ca2
Fiderana-17 <hei.fiderana@gmail.com>
2026-02-19 11:59:55 +0300

    feat: implement donneé, data and requetes

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -6,9 +6,3 @@ GRANT CONNECT ON DATABASE push_down_ceni TO ceni_db_manager;
 
 \c push_down_ceni
 
-GRANT CREATE ON SCHEMA public TO ceni_db_manager;
-
-
-GRANT SELECT ON invoice TO ceni_db_manager;
-GRANT SELECT ON invoice_line TO ceni_db_manager;
-GRANT SELECT ON tax_config TO ceni_db_manager;
