ab9383501ae3a08a9a731e8f851eb0b7134154ac
Fiderana-17 <hei.fiderana@gmail.com>
2026-02-23 15:14:33 +0300

    fix: Total number of votes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
         DBConnection dbConnection = new DBConnection();
 
         try (Connection connection = dbConnection.getConnection()) {
--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -6,3 +6,6 @@ GRANT CONNECT ON DATABASE push_down_ceni TO ceni_db_manager;
 
 \c push_down_ceni
 
+GRANT SELECT ON candidate TO ceni_db_manager;
+GRANT SELECT ON voter TO ceni_db_manager;
+GRANT SELECT ON vote TO ceni_db_manager;
